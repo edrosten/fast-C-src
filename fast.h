50f8541a72606731d1ac06378b8ca8d75e14769a
edrosten <>
2006-09-28 16:59:43 +0000

    Entering in to CVS

---
+++ fast.h
@@ -0,0 +1,12 @@
+#ifndef FAST_H
+#define FAST_H
+
+typedef struct { int x, y; } xy; 
+typedef unsigned char byte;
+
+xy*  fast_nonmax(const byte* im, int xsize, int ysize, xy* corners, int numcorners, int barrier, int* numnx);
+xy*  fast_corner_detect_9(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
+xy*  fast_corner_detect_10(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
+xy*  fast_corner_detect_11(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
+xy*  fast_corner_detect_12(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
+#endif
